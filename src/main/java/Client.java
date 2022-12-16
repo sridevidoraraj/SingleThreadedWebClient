759e57c08a0ab170840723d09166457ea3b5927d
Sridevi <sridevi.elangovan@gcitsolutions.com>
2022-12-16 09:41:52 +0530

    SingleThreadedWebClient - client program

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,31 +1,54 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
+import java.net.InetAddress;
 import java.net.Socket;
+import java.util.Arrays;
 
 public class Client {
     private final static String HOSTNAME = "localhost";
-    private final static int PORT = 8080;
+    private final static int PORT = 5000;
+    public static void main(String[] args) throws IOException {
+        System.out.println(Arrays.asList(args).toString());
+        File file = new File(args[0]);
+        try{
 
-    public static void main(String[] args) throws IOException
-    {
-        try (Socket clientSocket = new Socket(HOSTNAME, PORT);
-             InputStreamReader isr = new InputStreamReader(clientSocket.getInputStream());
-             BufferedReader in = new BufferedReader(isr);
-             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)
-        ) {
-            System.out.println("Connected to " + HOSTNAME + " on port " + PORT);
+        //Create Connection
+        Socket s = new Socket("localhost",5000);
+        System.out.println("[CONNECTED]");
+        DataOutputStream out = new DataOutputStream(s.getOutputStream());
+        DataInputStream in   = new DataInputStream(s.getInputStream());
 
-            String data = "Hello\nBye";
 
-            System.out.println("Sending to server:\n" + data);
-            out.println(data);
+        String header = "GET / HTTP/1.1\r\n" + file
+                +"Host:localhost\r\n\r\n";
+        byte[] byteHeader = header.getBytes();
+        out.write(byteHeader,0,header.length());
 
-            String line;
-            while ((line = in.readLine()) != null) {
-                System.out.println("Client received: " + line);
-            }
+        String res = "";
+        /////////////READ PROCESS/////////////
+        byte[] buf = new byte[in.available()];
+        in.readFully(buf);
+        System.out.println("\t[READ PROCESS]");
+        System.out.println("\t\tbuff length->"+buf.length);
+        for(byte b : buf)
+        {
+            res += (char) b;
         }
+        System.out.println("\t[/READ PROCESS]");
+        /////////////END READ PROCESS/////////////
+
+        System.out.println("[RES]");
+        System.out.println(res);
+        System.out.println("[CONN CLOSE]");
+
+        in.close();
+        out.close();
+        s.close();
+
+
+    }catch(Exception e)
+    {
+        e.printStackTrace();
     }
+    }
+
 }
