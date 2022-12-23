8716b86a7850320e714affb4539832de974b4736
Sridevi <sridevi.elangovan@gcitsolutions.com>
2022-12-23 15:06:48 +0530

    SingleThreadedWebClient - client program

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -4,51 +4,76 @@ import java.net.Socket;
 import java.util.Arrays;
 
 public class Client {
-    private final static String HOSTNAME = "localhost";
-    private final static int PORT = 5000;
-    public static void main(String[] args) throws IOException {
-        System.out.println(Arrays.asList(args).toString());
-        File file = new File(args[0]);
-        try{
+    public static void main(String[] args) {
 
-        //Create Connection
-        Socket s = new Socket("localhost",5000);
-        System.out.println("[CONNECTED]");
-        DataOutputStream out = new DataOutputStream(s.getOutputStream());
-        DataInputStream in   = new DataInputStream(s.getInputStream());
+        File file = new File("D:\\Projects\\csweb\\index.html");
+
+            try {
+
+                Socket s = new Socket("localhost", 8080);
+                System.out.println("[CONNECTED]");
+
+                DataInputStream in = new DataInputStream(s.getInputStream());
+                BufferedReader br = new BufferedReader(new FileReader(file));
+                String header = "GET / HTTP/1.0\r\n" + file
+                        + "Host:localhost\r\n\r\n";
+                byte[] byteHeader = header.getBytes();
+
+                DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+                dos.write(byteHeader, 0, header.length());
+                dos.writeUTF(header);
+                String res = "";
+
+                byte[] buf = new byte[in.available()];
+                in.readFully(buf);
+                System.out.println("\t[READ PROCESS]");
+                System.out.println("\t\tbuff length->" + buf.length);
+                for (byte b : buf) {
+                    res += (char) b;
+                }
+                System.out.println("\t[/READ PROCESS]");
 
 
-        String header = "GET / HTTP/1.1\r\n" + file
-                +"Host:localhost\r\n\r\n";
-        byte[] byteHeader = header.getBytes();
-        out.write(byteHeader,0,header.length());
+                System.out.println("[RES]");
+                System.out.println(res);
+                System.out.println("[CONN CLOSE]");
 
-        String res = "";
-        /////////////READ PROCESS/////////////
-        byte[] buf = new byte[in.available()];
-        in.readFully(buf);
-        System.out.println("\t[READ PROCESS]");
-        System.out.println("\t\tbuff length->"+buf.length);
-        for(byte b : buf)
-        {
-            res += (char) b;
+                in.close();
+                dos.close();
+                s.close();
+
+
+//        String header = "GET / HTTP/1.0\r\n" + file
+//                +"Host:localhost\r\n\r\n";
+//        byte[] byteHeader = header.getBytes();
+//        out.write(byteHeader,0,header.length());
+//
+//        String res = "";
+//
+//        byte[] buf = new byte[in.available()];
+//        in.readFully(buf);
+//        System.out.println("\t[READ PROCESS]");
+//        System.out.println("\t\tbuff length->"+buf.length);
+//        for(byte b : buf)
+//        {
+//            res += (char) b;
+//        }
+//        System.out.println("\t[/READ PROCESS]");
+//
+//
+//        System.out.println("[RES]");
+//        System.out.println(res);
+//        System.out.println("[CONN CLOSE]");
+//
+//        in.close();
+//        out.close();
+//        s.close();
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
-        System.out.println("\t[/READ PROCESS]");
-        /////////////END READ PROCESS/////////////
 
-        System.out.println("[RES]");
-        System.out.println(res);
-        System.out.println("[CONN CLOSE]");
-
-        in.close();
-        out.close();
-        s.close();
-
-
-    }catch(Exception e)
-    {
-        e.printStackTrace();
-    }
-    }
 
 }
